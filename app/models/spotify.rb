b24cefd552f7527ca91b81ea621f2a1cc24099ae
Adrian Maseda <amaseda@gmail.com>
2017-05-23 15:56:32 -0400

    create! => create

--- app/models/spotify.rb
+++ app/models/spotify.rb
@@ -17,7 +17,7 @@ class Spotify
   end
 
   def self.create_playlist_genres_and_artists(playlist_data, playlist_params, artists_array, genres_array)
-    new_playlist = Playlist.create!(
+    new_playlist = Playlist.create(
       name: playlist_params["name"],
       playlist_url: playlist_params["playlist_url"],
       image_url: playlist_data["images"][0]["url"]
@@ -29,20 +29,20 @@ class Spotify
     # genres_array.each do |genre|
     #   current_genre = Genre.find_by(name: genre)
     #   if(current_genre)
-    #     PlaylistGenre.create!(playlist: new_playlist, genre: current_genre)
+    #     PlaylistGenre.create(playlist: new_playlist, genre: current_genre)
     #   else
-    #     new_genre = Genre.create!(name: genre)
-    #     PlaylistGenre.create!(playlist: new_playlist, genre: new_genre)
+    #     new_genre = Genre.create(name: genre)
+    #     PlaylistGenre.create(playlist: new_playlist, genre: new_genre)
     #   end
     # end
 
     # artists_array.each do |artist|
     #   current_artist = Artist.find_by(name: artist)
     #   if(current_artist)
-    #     PlaylistArtist.create!(playlist: new_playlist, artist: current_artist)
+    #     PlaylistArtist.create(playlist: new_playlist, artist: current_artist)
     #   else
-    #     new_artist = Artist.create!(name: artist)
-    #     PlaylistArtist.create!(playlist: new_playlist, artist: new_artist)
+    #     new_artist = Artist.create(name: artist)
+    #     PlaylistArtist.create(playlist: new_playlist, artist: new_artist)
     #   end
     # end
   end
@@ -51,10 +51,10 @@ class Spotify
     data.each do |item|
       current_item = base_model.find_by(name: item)
       if(current_item)
-        join_model.create!(playlist: new_playlist, base_model.to_s.downcase => current_item)
+        join_model.create(playlist: new_playlist, base_model.to_s.downcase => current_item)
       else
-        new_item = base_model.create!(name: item)
-        join_model.create!(playlist: new_playlist, base_model.to_s.downcase => new_item)
+        new_item = base_model.create(name: item)
+        join_model.create(playlist: new_playlist, base_model.to_s.downcase => new_item)
       end
     end
   end
